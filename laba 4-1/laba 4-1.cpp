439b06e7c38fe7718aec208342bbb944c9678547
streltsov <167978599+lomonosovkrut@users.noreply.github.com>
2024-10-04 10:35:00 +0300

    Add files via upload

---
+++ laba 4-1/laba 4-1.cpp
@@ -0,0 +1,98 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Функция для создания дерева
+struct Node* CreateTree(struct Node* root, struct Node* r, int data) {
+    if (r == NULL) {
+        r = (struct Node*)malloc(sizeof(struct Node));
+        if (r == NULL) {
+            printf("Ошибка выделения памяти");
+            exit(0);
+        }
+
+        r->left = NULL;
+        r->right = NULL;
+        r->data = data;
+        if (root == NULL) return r;
+
+        if (data > root->data) root->left = r;
+        else root->right = r;
+        return r;
+    }
+
+    if (data > r->data)
+        CreateTree(r, r->left, data);
+    else
+        CreateTree(r, r->right, data);
+
+    return root;
+}
+
+// Функция для вывода дерева
+void print_tree(struct Node* r, int l) {
+    if (r == NULL) {
+        return;
+    }
+
+    print_tree(r->right, l + 1);
+    for (int i = 0; i < l; i++) {
+        printf(" ");
+    }
+
+    printf("%d\n", r->data);
+    print_tree(r->left, l + 1);
+}
+
+// Функция для поиска значения в дереве
+struct Node* search(struct Node* root, int value) {
+    if (root == NULL || root->data == value) {
+        return root;
+    }
+    if (value < root->data) {
+        return search(root->left, value);
+    }
+    else {
+        return search(root->right, value);
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+    int D, start = 1;
+    struct Node* root = NULL;
+
+    printf("-1 - окончание построения дерева\n");
+    while (start) {
+        printf("Введите число: ");
+        scanf("%d", &D);
+        if (D == -1) {
+            printf("Построение дерева окончено\n\n");
+            start = 0;
+        }
+        else {
+            root = CreateTree(root, root, D);
+        }
+    }
+
+    print_tree(root, 0);
+
+    printf("Введите значение для поиска: ");
+    scanf("%d", &D);
+    struct Node* found = search(root, D);
+    if (found != NULL) {
+        printf("Значение %d найдено в дереве.\n", D);
+    }
+    else {
+        printf("Значение %d не найдено в дереве.\n", D);
+    }
+
+    return 0;
+}
